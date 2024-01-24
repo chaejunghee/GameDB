aada740e3fcfe3ff2fa7c9be4a69ff34a8827c73
usuns <usunsun38@naver.com>
2024-01-25 08:57:17 +0900

    Game 클래스 테스트

---
+++ app/src/main/java/com/example/myapplication/Game.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+open class Game : View {
+    var scrw = 0  //스크린 너비 변수
+    var scrh = 0  //스크린 높이 변수
+    var x = 0     //캐릭터 x좌표
+    var y = 0     //캐릭터 y좌표
+
+    val paint = Paint()
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    //뷰 크기가 변경되면 호출되는 메소드
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        this.scrw = w   //현재 스크린 너비값 설정
+        this.scrh = h   //현재 스크린 높이값 설정
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.color = Color.BLACK
+        paint.textSize = scrh / 16f
+        canvas?.drawText("test", 0f, scrh / 16f, paint)
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn_left: Button
+    lateinit var btn_right: Button
+    lateinit var btn_up: Button
+    lateinit var btn_down: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_left = findViewById(R.id.btn_left)
+        btn_right = findViewById(R.id.btn_right)
+        btn_up = findViewById(R.id.btn_up)
+        btn_down = findViewById(R.id.btn_down)
     }
 }
